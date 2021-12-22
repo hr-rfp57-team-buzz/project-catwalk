43b591126b6a409fe29512c807acedf4a5f221cc
Ryne Cichella <rcichella@csmcentral.com>
2021-12-22 12:15:07 -0800

    Added App.jsx for react app

---
+++ client/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state ={
+
+    }
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h1>Simple App</h1>
+      </div>
+    )
+  }
+}
+
+
+
+
+export default App;

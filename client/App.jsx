605fac45cfd09994605839ea6590ad4dc1ea8975
Alex Romero <alexromeroreyes09@gmail.com>
2021-12-22 16:55:22 -0800

    Added wireframe for our web app

--- client/App.jsx
+++ client/App.jsx
@@ -9,11 +9,23 @@ class App extends React.Component {
     };
   }
 
-
   render() {
     return (
-      <div>
+      <div id="container">
         <h1>Simple App</h1>
+        <div id="product-overview">
+          <div className="po-image">Product Picture</div>
+          <div className="po-details"></div>
+        </div>
+        <div id="related-products">Related Products</div>
+          <div className="related"> Related</div>
+          <div className="your-outfit">Outfit</div>
+        <div id="questions-answers">Questions &amp; Answers</div>
+        <div id="ratings-reviews">
+          Ratings &amp; Reviews
+          <div className="ratingOverview"> Rating overview</div>
+          <div className="review">Review</div>
+        </div>
       </div>
     );
   }

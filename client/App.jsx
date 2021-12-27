58aae169e9050cd6b4c1d4adcb6ec1fb09c32b6a
Alex Romero <alexromeroreyes09@gmail.com>
2021-12-27 09:09:32 -0800

    Rebased from previous commit

--- client/App.jsx
+++ client/App.jsx
@@ -1,21 +1,11 @@
 import React from 'react';
 import Overview from './Overview.jsx';
-<<<<<<< HEAD
-<<<<<<< HEAD
-import RatingReviews from './RatingReviews.jsx';
-=======
-import QuestionsAndAnswers from './QuestionsAndAnswers.jsx';
->>>>>>> cc8abc0 (Added Questions and Answers widget along with individual module components)
-=======
 import QuestionsAndAnswers from './Q&A/QuestionsAndAnswers.jsx';
->>>>>>> b8e1a7b (Added hardcoded data to format question card)
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
-
     };
   }
 
@@ -28,16 +18,12 @@ class App extends React.Component {
           <div className="related"> Related</div>
           <div className="your-outfit">Outfit</div>
         <div id="questions-answers">Questions &amp; Answers</div>
-<<<<<<< HEAD
-        <RatingReviews />
-=======
         <QuestionsAndAnswers />
         <div id="ratings-reviews">
           Ratings &amp; Reviews
           <div className="ratingOverview"> Rating overview</div>
           <div className="review">Review</div>
         </div>
->>>>>>> cc8abc0 (Added Questions and Answers widget along with individual module components)
       </div>
     );
   }

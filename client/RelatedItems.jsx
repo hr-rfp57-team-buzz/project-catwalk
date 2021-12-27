9dbb2a02c9a5eecf5f7931f9a8340eced7523b5d
Joseph Corral <genobeam125@gmail.com>
2021-12-27 12:12:09 -0800

    added fake data to cards

--- client/RelatedItems.jsx
+++ client/RelatedItems.jsx
@@ -2,13 +2,18 @@ import React, {useState} from 'react';
 
 
 var Related = () => {
-
+  const [x, setX] = useState(50);
+  const styles = {
+    transformRelated: `translate(${x}px, 0px)`
+  };
   return (
     <div>
-      <div className="reel">
+      <div className="reel"style={styles}onClick={()=>setX(x + 50)}>
+        <Cards/>
+        <Cards/>
         <Cards/>
       </div>
-      <div className="reel">
+      <div className="reel"style={styles}onClick={()=>setX(x + 50)}>
         <Cards/>
       </div>
     </div>
@@ -16,14 +21,18 @@ var Related = () => {
 };
 
 var Cards = () => {
-  const [x, setX] = useState(50);
-  const styles = {
-    transform: `translate(${x}px, 0px)`
-  };
+
 
   return (
-    <div className="Card" style={styles}onClick={()=>setX(x + 50)}>
-      Hello
+    <div className="Card" >
+      <div className="relatedPicHolder">
+        <img className="relatedPic" src="https://images.unsplash.com/photo-1501088430049-71c79fa3283e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=300&amp;q=80"></img>
+      </div>
+      <div className="relatedTextHolder">
+        <h3>Jacket</h3>
+        <h4>Sportswear</h4>
+        <h4>$29.99</h4>
+      </div>
     </div>
   );
 };

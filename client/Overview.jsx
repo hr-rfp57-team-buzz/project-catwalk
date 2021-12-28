800e23e1dd37aecc501e8f9e6602f16db49a9d2b
Daniel <tawata.daniel@gmail.com>
2021-12-28 00:27:04 -0800

    Functial classes setup finished

--- client/Overview.jsx
+++ client/Overview.jsx
@@ -1,83 +1,64 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React from 'react';
+import OverviewImage from './OverviewImage.jsx';
 import OverviewDetails from './OverviewDetails.jsx';
-import {OverviewContext} from './OverviewContext.jsx';
 import axios from 'axios';
 
-const Overview = () => {
-  // const [photo, setPhoto] = useState('photograph');
-  const [currentProduct, setCurrentProduct] = useState([]);
-  const [products, setProducts, styles, setStyles] = useContext(OverviewContext);
+class Overview extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      product: {},
+      variations: []
+    };
+    this.handleStyleChange = this.handleStyleChange.bind(this);
+  }
 
-  useEffect(() => {
-    axios.get('/products')
+  componentDidMount() {
+    axios.get('http://localhost:3000/products')
       .then((res) => {
         console.log(res.data);
-        setCurrentProduct(res.data[0]);
-        console.log(res.data[0].id);
-        const url = '/products/' + res.data[0].id + '/styles';
-        axios.get(url)
-          .then((res) => {
-            console.log(res.data.results);
-            setStyles(res.data.results);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
+        const id = res.data[0].id;
+        return axios.get(`http://localhost:3000/products/${id}`);
+      })
+      .then((res) => {
+        console.log(res.data);
+        this.setState({product: res.data});
+      })
+      .then((res) => {
+        return axios.get('http://localhost:3000/products/40344/styles');
+      })
+      .then((res) => {
+        for (let i = 0; i < res.data.results.length; i++) {
+          res.data.results[i].selected = false;
+        }
+        res.data.results[0].selected = true;
+        this.setState({variations: res.data.results});
+      })
+      .catch((err) => {
+        console.log(err);
       });
-  }, []);
+  }
 
-  return (
-    <div id="product-overview">
-      <div className="announcement">Site-Wide Announcement Message! -- Sale / Discount <strong>Offer</strong> -- <u>New Product Highlight</u></div>
-      <div className="po-flex">
-        <OverviewImage />
-        <OverviewDetails product={currentProduct} fashion={styles} />
+  handleStyleChange(index) {
+    console.log('hello')
+    // for (let i = 0; i < this.state.variations.length; i++) {
+    //   variations[i].selected = false;
+    // }
+    // variations[index].selected = true;
+    // this.setState({variations: variations});
+  }
+
+  render() {
+    return (
+      <div id="product-overview">
+        <div className="po-announcement">Site-Wide Announcement Message! -- Sale / Discount <strong>Offer</strong> -- <u>New Product Highlight</u></div>
+        <div className="po-flex">
+          <OverviewImage />
+          <OverviewDetails product={this.state.product} variations={this.state.variations} handleStyleChange={this.handleStyleChange} />
+        </div>
       </div>
-    </div>
-  );
-};
-
-const carousel = () => {
-  console.log('clicked');
-};
-
-
-const OverviewImage = (props) => {
-  // const rightArrow = document.querySelector('.po-right-arrow');
-  // rightArrow.addEventListener('click', carousel);
-  let counter = 0;
-  const prevSlide = () => {
-    const slideReel = document.querySelector('.slide-reel');
-    counter--;
-    const move = (counter * -100) + '%';
-    slideReel.style.transform = 'translateX(' + move + ')';
-  };
-
-  const nextSlide = () => {
-    const slideReel = document.querySelector('.slide-reel');
-    counter++;
-    const move = (counter * -100) + '%';
-    slideReel.style.transform = 'translateX(' + move + ')';
-  };
-
-  return (
-    <div className="po-image">
-      <div onClick={prevSlide} className="po-left-arrow"><i className="fas fa-arrow-left"></i></div>
-      <div onClick={nextSlide} className="po-right-arrow"><i className="fas fa-arrow-right"></i></div>
-      <div className="po-mini-images">
-        <div className="po-mini-indv"></div>
-        <div className="po-mini-indv"></div>
-        <div className="po-mini-indv"></div>
-        <div className="po-mini-indv"></div>
-        <div className="po-mini-indv"></div>
-      </div>
-      <div className="slide-reel">
-        <img src="https://images.unsplash.com/photo-1532543491484-63e29b3c1f5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80" />
-        <img src="https://images.unsplash.com/photo-1532543491484-63e29b3c1f5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80" />
-        <img src="https://images.unsplash.com/photo-1532543491484-63e29b3c1f5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80" />
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Overview;
--- client/OverviewStyles.jsx
+++ client/OverviewStyles.jsx
@@ -1,30 +1,108 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-const OverviewStyles = (props) => {
-  return (
-    <div className="po-styles">
-      {
-        props.fashion.map((item) => {
-          return (<OverviewStyle fashion={item} key={Math.random().toFixed(8)} />);
-        })
-      }
-    </div>
-  );
-};
+class OverviewStyles extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-const OverviewStyle = (props) => {
-  let styling;
-  if (props.fashion) {
-    styling = {
-      'background': 'url(' + props.fashion.photos[0].thumbnail_url + ')',
+
+  render() {
+    const styleChanges = this.props.handleStyleChange;
+    return (
+      <div className="po-styles">
+        {
+          this.props.variations.map((variation, index) => {
+            return (<OverviewStyle key={variation.style_id} variation={variation} index={index} styleChanges={styleChanges} />);
+          })
+        }
+      </div>
+    );
+  }
+}
+
+class OverviewStyle extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+
+    let styling = {
+      'background': 'url(' + this.props.variation.photos[0].thumbnail_url + ')',
       'backgroundSize': 'cover',
       'backgroundPosition': 'center'
     };
-  }
+    let checkmark = 'po-style-check active';
+    this.props.variation.selected ? checkmark = 'po-style-check active' : checkmark = 'po-style-check';
 
-  return (
-    <div className="po-style" style={styling}></div>
-  );
-};
+    return (
+      <div className="po-style" style={styling} onClick={this.styleChanges} >
+        <div className={checkmark}><i className="fas fa-check"></i></div>
+      </div>
+    );
+  }
+}
+// const OverviewStyles = (props) => {
+//   const initial = props.fashion || [];
+//   if (initial) {
+//     for (let i = 0; i < initial.length; i++) {
+//       initial[i].selected = false;
+//       if (i === 1) {
+//         initial[i].selected = true;
+//       }
+//     }
+//     // initial[0].selected = false;
+//   }
+//   const [fashion, setFashion] = useState(initial);
+//   useEffect(() => {
+//     setFashion(initial);
+//   });
+
+//   const handleReset = (index) => {
+//     console.log('eresting');
+//     const newNew = initial.slice();
+//     for (let i = 0; i < newNew.length; i++) {
+//       newNew[i].selected = true;
+//     }
+//     setFashion(newNew);
+//   };
+
+//   return (
+
+//   );
+// };
+
+// const OverviewStyle = (props) => {
+
+
+//   const [check, setCheck] = useState(checking);
+
+//   // if (props.fashion.selected) {
+//   //   setCheck('po-style-check active');
+//   // }
+
+//   // if (props.chosen === 'true') {
+//   //   setCheck('po-style-check active');
+//   // } else {
+//   //   setCheck('po-style-check');
+//   // }
+
+
+//   const toggleCheck = () => {
+//     console.log('hello')
+//     props.handleReset(2);
+//     // if (check === 'po-style-check') {
+//     //   setCheck('po-style-check active');
+//     // } else {
+//     //   setCheck('po-style-check');
+//     // }
+//   };
+
+
+//   return (
+
+//   );
+// };
 
 export default OverviewStyles;

8ae3552fe2a88a9dd3d633adf4c36b8ee7d97cd1
Ryne Cichella <rcichella@csmcentral.com>
2021-12-28 15:33:10 -0800

    merge success

--- client/App.jsx
+++ client/App.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import Overview from './Overview/Overview.jsx';
 import QuestionsAndAnswers from './Q&A/QuestionsAndAnswers.jsx';
-<<<<<<< HEAD
 import RatingReviews from './RatingReviews/RatingReviews.jsx';
 
-=======
 import Related from './RelatedItems.jsx';
->>>>>>> main
 
 class App extends React.Component {
   constructor(props) {
@@ -21,13 +18,9 @@ class App extends React.Component {
         <h1>Simple App</h1>
         <Overview />
         <div id="related-products">Related Products</div>
-<<<<<<< HEAD
-        <div className="related"> Related</div>
-=======
         <div className="related">
           <Related />
         </div>
->>>>>>> main
         <div className="your-outfit">Outfit</div>
         <div id="questions-answers">Questions &amp; Answers</div>
         <QuestionsAndAnswers />

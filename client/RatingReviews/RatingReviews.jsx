73f5a0ea64fd6168b439b5699cd5ac6a89c5bdc5
Ryne Cichella <rcichella@csmcentral.com>
2021-12-28 10:55:50 -0800

    Generate Average Rating for product successful

--- client/RatingReviews/RatingReviews.jsx
+++ client/RatingReviews/RatingReviews.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Rating from './Rating.jsx';
 import Review from './Review.jsx';
@@ -6,9 +6,11 @@ import Review from './Review.jsx';
 let RatingReview = () => {
   //captain HOOKS
   let [reviews, setReviews] = useState([0]);
+  let [reviewMeta, setReviewMeta] = useState(undefined);
+  let [averageRating, setAverageRating] = useState('Loading...');
 
   //to be hooked up to productId context
-  let prodId = 40345;
+  let prodId = 40344;
 
   let getProdReviews = (prodId) => {
     axios.get(`/reviews/${prodId}`)
@@ -21,13 +23,45 @@ let RatingReview = () => {
       });
   };
 
-  React.useEffect(() => {
+  let getProdReviewMeta = (prodId) => {
+    axios.get(`/reviews/${prodId}/meta`)
+      .then((res) => {
+        // console.log(res.data);
+        setReviewMeta(res.data);
+        let average = 0;
+        let score = 0;
+        for (var key in res.data.ratings) {
+          average += Number(res.data.ratings[key]);
+          score += Number(key) * Number(res.data.ratings[key]);
+        }
+        setAverageRating(Number(score / average).toFixed(1));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  // let generateAverageRating = () => {
+  //   console.log('THIS LOGGED', reviewMeta.ratings);
+  //   if (reviewMeta.ratings !== undefined) {
+  //     let average = 0;
+  //     for (var key in reviewMeta.ratings) {
+  //       average += reviewMeta.ratings[key];
+  //     }
+  //     setAverageRating(average);
+  //   } else {
+  //     return;
+  //   }
+  // };
+
+  useEffect(() => {
     getProdReviews(prodId);
+    getProdReviewMeta(prodId);
   }, []);
 
   return (
     <div id="ratings-reviews">
-      <Rating />
+      <Rating reviewMeta={reviewMeta} averageRating={averageRating} />
       <Review reviews={reviews} />
     </div>
   );
--- client/RatingReviews/ReviewListEntry.jsx
+++ client/RatingReviews/ReviewListEntry.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 let ReviewListEntry = (props) => {
 
@@ -11,7 +11,7 @@ let ReviewListEntry = (props) => {
   };
 
 
-  React.useEffect(() => {
+  useEffect(() => {
     createPhotoArray();
   }, []);
 

690c2a50103d848a82879c7e5b80b4698503d773
Joseph Corral <genobeam125@gmail.com>
2021-12-28 12:34:10 -0800

    Completed arrow positioning/functionality

--- client/RelatedItems.jsx
+++ client/RelatedItems.jsx
@@ -2,22 +2,41 @@ import React, {useState} from 'react';
 
 
 var Related = () => {
-  const [x, setX] = useState(50);
-  const [x2, setX2] = useState(50);
+  const [x, setX] = useState(0);
+  const [x2, setX2] = useState(0);
   const stylesRelated = {
     transform: `translate(${x}px, 0px)`
   };
   const stylesOutfit = {
     transform: `translate(${x2}px, 0px)`
   };
+
+  var moveLeft = function() {
+    if (x > 0) {
+      setX(x - 340);
+    }
+  };
+
+  var moveLeftOutfit = function() {
+    if (x2 > 0) {
+      setX2(x2 - 340);
+    }
+  };
+
   return (
     <div>
-      <div className="reel"style={stylesRelated}onClick={()=>setX(x + 50)}>
+      <i class="fas fa-arrow-right rArrow" onClick={()=>setX(x + 340)}></i>
+      <i class="fas fa-arrow-left lArrow" onClick={()=>moveLeft()}></i>
+      <div className="reel"style={stylesRelated}>
+        <Cards/>
+        <Cards/>
         <Cards/>
         <Cards/>
         <Cards/>
       </div>
-      <div className="reel"style={stylesOutfit}onClick={()=>setX2(x2 + 50)}>
+      <i className="lArrow" class="fas fa-arrow-left lArrow2" onClick={()=> moveLeftOutfit()}></i>
+      <i className="rArrow" class="fas fa-arrow-right rArrow2" onClick={()=>setX2(x2 + 340)}></i>
+      <div className="reel"style={stylesOutfit}>
         <Cards/>
       </div>
     </div>
@@ -33,15 +52,11 @@ var Cards = () => {
         <img className="relatedPic" src="https://images.unsplash.com/photo-1501088430049-71c79fa3283e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=300&amp;q=80"></img>
       </div>
       <div className="relatedTextHolder">
-        <h3>Jacket</h3>
-        <h4>Sportswear</h4>
-        <h4>$29.99</h4>
+        <p id="relatedCategory">Sportswear</p>
+        <h3>Forest Green Jacket With Detachable Hood</h3>
+        <p>$29.99</p>
+        <p>5 Stars</p>
       </div>
-
-
-  return (
-    <div className="Card" style={styles}onClick={()=>setX(x + 50)}>
-      Hello
     </div>
   );
 };

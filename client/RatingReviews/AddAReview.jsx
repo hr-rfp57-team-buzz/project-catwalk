e2a3a92f4d54c39d1036818d042dfd52de1b8d03
Ryne Cichella <rcichella@csmcentral.com>
2022-01-02 10:47:11 -0800

    refactor to use hooks to use state to keep track of selected info when submitting a review

--- client/RatingReviews/AddAReview.jsx
+++ client/RatingReviews/AddAReview.jsx
@@ -7,20 +7,19 @@ import AddAReviewCharacteristics from './AddAReviewCharacteristics.jsx';
 let AddAReview = ({window, prodId, reviewMeta, scrape}) => {
 
   //captain HOOKs
+  let [sizeRatingSelection, setSize] = useState(undefined);
+  let [widthRatingSelection, setWidth] = useState(undefined);
+  let [comfortRatingSelection, setComfort] = useState(undefined);
+  let [qualityRatingSelection, setQuality] = useState(undefined);
+  let [lengthRatingSelection, setLength] = useState(undefined);
+  let [fitRatingSelection, setFit] = useState(undefined);
+  let [doYouRecommendSelection, setRecommend] = useState(undefined);
+
   let [productRating, setProductRating] = useState(0);
   let [minCharCount, setMinCharCount] = useState(50);
-  // let [charCount, setCharCount] = useState(
-  //   <p id="reviewBodyMin">Minimum required characters left: {minCharCount}</p>
-  // );
+
   let [charCount, setCharCount] = useState( `Minimum required characters left: ${minCharCount}` );
 
-  let sizeRatingSelection = 0;
-  let widthRatingSelection = 0;
-  let comfortRatingSelection = 0;
-  let qualityRatingSelection = 0;
-  let lengthRatingSelection = 0;
-  let fitRatingSelection = 0;
-  let doYouRecommendSelection;
   let reviewSummary = document.getElementById('reviewSummary');
   let reviewBody = document.getElementById('reviewBody');
   let reviewNickname = document.getElementById('reviewNickname');
@@ -29,35 +28,41 @@ let AddAReview = ({window, prodId, reviewMeta, scrape}) => {
 
   let doYouRecommend = (e) => {
     if (e.target.value === 'false') {
-      doYouRecommendSelection = false;
+      setRecommend(false);
     }
     if (e.target.value === 'true') {
-      doYouRecommendSelection = true;
+      setRecommend(true);
     }
   };
 
   let sizeRating = (e) => {
-    sizeRatingSelection = e.target.value;
+    let size = e.target.value;
+    setSize(size);
   };
 
   let widthRating = (e) => {
-    widthRatingSelection = e.target.value;
+    let width = e.target.value;
+    setWidth(width);
   };
 
   let comfortRating = (e) => {
-    comfortRatingSelection = e.target.value;
+    let comfort = e.target.value;
+    setComfort(comfort);
   };
 
   let qualityRating = (e) => {
-    qualityRatingSelection = e.target.value;
+    let quality = e.target.value;
+    setQuality(quality);
   };
 
   let lengthRating = (e) => {
-    lengthRatingSelection = e.target.value;
+    let length = e.target.value;
+    setLength(length);
   };
 
   let fitRating = (e) => {
-    fitRatingSelection = e.target.value;
+    let fit = e.target.value;
+    setFit(fit);
   };
 
 
@@ -66,7 +71,6 @@ let AddAReview = ({window, prodId, reviewMeta, scrape}) => {
   };
 
   let minimumCharacterCount = (e) => {
-    // SOMETHING IN THIS FUNCTION IS BREAKING AND RESETTING THE STATE OF THE HOOKS ABOVE
     e.preventDefault();
     let charsLeft = 50 - e.target.value.length;
     if (charsLeft < 1) {
@@ -139,10 +143,6 @@ let AddAReview = ({window, prodId, reviewMeta, scrape}) => {
       });
   };
 
-  // useEffect(() => {
-  //   // console.log(productRating);
-  // }, [productRating]);
-
 
 
 
@@ -177,162 +177,7 @@ let AddAReview = ({window, prodId, reviewMeta, scrape}) => {
             </div>
           </div>
           <p>Characteristics *</p>
-          {/* <table className="reviewAddTable">
-            <tbody>
-              <tr>
-                <th></th>
-                <th>1</th>
-                <th>2</th>
-                <th>3</th>
-                <th>4</th>
-                <th>5</th>
-              </tr> */}
           <AddAReviewCharacteristics sizeRating={sizeRating} widthRating={widthRating} comfortRating={comfortRating} qualityRating={qualityRating} lengthRating={lengthRating} fitRating={fitRating} reviewMeta={reviewMeta} scrape={scrape}/>
-          {/* <tr onChange={sizeRating}>
-                <td><b>Size</b></td>
-                <td>
-                  <input type="radio" id="sizeAdd1" name="sizeReview" value="1"></input>
-                  <p>A size too small</p>
-                </td>
-                <td>
-                  <input type="radio" id="sizeAdd2" name="sizeReview" value="2"></input>
-                  <p>1/2 size too small</p>
-                </td>
-                <td>
-                  <input type="radio" id="sizeAdd3" name="sizeReview" value="3"></input>
-                  <p>Perfect</p>
-                </td>
-                <td>
-                  <input type="radio" id="sizeAdd4" name="sizeReview" value="4"></input>
-                  <p>1/2 size too big</p>
-                </td>
-                <td>
-                  <input type="radio" id="sizeAdd5" name="sizeReview" value="5"></input>
-                  <p>A size too wide</p>
-                </td>
-              </tr>
-
-              <tr onChange={widthRating}>
-                <td><b>Width</b></td>
-                <td>
-                  <input type="radio" id="widthAdd1" name="widthReview" value="1"></input>
-                  <p>Too narrow</p>
-                </td>
-                <td>
-                  <input type="radio" id="widthAdd2" name="widthReview" value="2"></input>
-                  <p>Slightly narrow</p>
-                </td>
-                <td>
-                  <input type="radio" id="widthAdd3" name="widthReview" value="3"></input>
-                  <p>Perfect</p>
-                </td>
-                <td>
-                  <input type="radio" id="widthAdd4" name="widthReview" value="4"></input>
-                  <p>Slightly wide</p>
-                </td>
-                <td>
-                  <input type="radio" id="widthAdd5" name="widthReview" value="5"></input>
-                  <p>Too wide</p>
-                </td>
-              </tr>
-
-              <tr onChange={comfortRating}>
-                <td><b>Comfort</b></td>
-                <td>
-                  <input type="radio" id="comfortAdd1" name="comfortReview" value="1"></input>
-                  <p>Uncomfortable</p>
-                </td>
-                <td>
-                  <input type="radio" id="comfortAdd2" name="comfortReview" value="2"></input>
-                  <p>Slightly uncomfortable</p>
-                </td>
-                <td>
-                  <input type="radio" id="comfortAdd3" name="comfortReview" value="3"></input>
-                  <p>Ok</p>
-                </td>
-                <td>
-                  <input type="radio" id="comfortAdd4" name="comfortReview" value="4"></input>
-                  <p>Comfortable</p>
-                </td>
-                <td>
-                  <input type="radio" id="comfortAdd5" name="comfortReview" value="5"></input>
-                  <p>Perfect</p>
-                </td>
-              </tr>
-
-              <tr onChange={qualityRating}>
-                <td><b>Quality</b></td>
-                <td>
-                  <input type="radio" id="qualityAdd1" name="qualityReview" value="1"></input>
-                  <p>Poor</p>
-                </td>
-                <td>
-                  <input type="radio" id="qualityAdd2" name="qualityReview" value="2"></input>
-                  <p>Below Average</p>
-                </td>
-                <td>
-                  <input type="radio" id="qualityAdd3" name="qualityReview" value="3"></input>
-                  <p>What I expected</p>
-                </td>
-                <td>
-                  <input type="radio" id="qualityAdd4" name="qualityReview" value="4"></input>
-                  <p>Pretty great</p>
-                </td>
-                <td>
-                  <input type="radio" id="qualityAdd5" name="qualityReview" value="5"></input>
-                  <p>Perfect</p>
-                </td>
-              </tr>
-
-              <tr onChange={lengthRating}>
-                <td><b>Length</b></td>
-                <td>
-                  <input type="radio" id="lengthAdd1" name="lengthReview" value="1"></input>
-                  <p>Runs Short</p>
-                </td>
-                <td>
-                  <input type="radio" id="lengthAdd2" name="lengthReview" value="2"></input>
-                  <p>Runs slightly short</p>
-                </td>
-                <td>
-                  <input type="radio" id="lengthAdd3" name="lengthReview" value="3"></input>
-                  <p>Perfect</p>
-                </td>
-                <td>
-                  <input type="radio" id="lengthAdd4" name="lengthReview" value="4"></input>
-                  <p>Runs slightly long</p>
-                </td>
-                <td>
-                  <input type="radio" id="lengthAdd5" name="lengthReview" value="5"></input>
-                  <p>Runs long</p>
-                </td>
-              </tr>
-
-              <tr onChange={fitRating}>
-                <td><b>Fit</b></td>
-                <td>
-                  <input type="radio" id="fitAdd1" name="fitReview" value="1"></input>
-                  <p>Runs tight</p>
-                </td>
-                <td>
-                  <input type="radio" id="fitAdd2" name="fitReview" value="2"></input>
-                  <p>Runs slightly tight</p>
-                </td>
-                <td>
-                  <input type="radio" id="fitAdd3" name="fitReview" value="3"></input>
-                  <p>Perfect</p>
-                </td>
-                <td>
-                  <input type="radio" id="fitAdd4" name="fitReview" value="4"></input>
-                  <p>Runs slightly long</p>
-                </td>
-                <td>
-                  <input type="radio" id="fitAdd5" name="fitReview" value="5"></input>
-                  <p>Runs long</p>
-                </td>
-              </tr> */}
-          {/* </tbody>
-          </table> */}
           <p>Review Summary</p>
           <textarea id="reviewSummary" name="reviewSummary" rows="2" cols="50" maxLength="60" placeholder="Example: Best purchase ever!"></textarea>
           <p>Review body *</p>

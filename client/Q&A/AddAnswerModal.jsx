427e07278d6e8fedb5b8c322ef8cde874862b794
Alex Romero <alexromeroreyes09@gmail.com>
2022-01-03 16:15:16 -0800

    Added Cloudinary to supoort photo upload

--- client/Q&A/AddAnswerModal.jsx
+++ client/Q&A/AddAnswerModal.jsx
@@ -1,20 +1,54 @@
 import React from 'react';
 
-let AddAnswerModal = (props) => (
-  <div>
-    <form className='answerModal answerAdd'>
-      <h1>Submit Your Answer</h1>
-      <h3> {props.prodName} : {props.qBody}</h3>
-      <label>Your Answer:</label>
-      <textarea maxlength='1000' rows="10" cols='100'>
-      </textarea>
-      <label>Nickname: </label>
-      <input placeholder='Example: jack543!' type='text'/>
-      <h5>For privacy reasons, do not use yout full name or email address</h5>
-      <label>Your email:</label>
-      <input type='text' maxlength='60'/>
-    </form>
-  </div>
-);
+class AddAnswerModal extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      images: []
+    }
+  }
+
+  showWidget = (widget) => {
+    widget.open();
+  }
+
+  render() {
+    let widget = window.cloudinary.createUploadWidget({
+      cloudName: 'arome9',
+      uploadPreset: 'ml_default'
+    }, (error, results) => {
+      if (error) {
+        console.log(error);
+      } else {
+        if (results) {
+          let photos = results.data.info.files;
+        }
+        // console.log(photo);
+        this.setState({
+          images: [...this.state.images, photos]
+        })
+      }
+    })
+    return (
+      <div>
+        <form className='answerModal answerAdd'>
+          <h1>Submit Your Answer</h1>
+          <h3> {this.props.prodName} : {this.props.qBody}</h3>
+          <label>Your Answer:</label>
+          <textarea maxlength='1000' rows="10" cols='100'>
+          </textarea>
+          <label>Nickname: </label>
+          <input placeholder='Example: jack543!' type='text'/>
+          <h5>For privacy reasons, do not use yout full name or email address</h5>
+          <label>Your email:</label>
+          <input placeholder='Example: jack@email.com' type='text' maxlength='60'/>
+          <h5>For authentication reasons, you will not be emailed</h5>
+          <button onClick={(e) => {e.preventDefault(); this.showWidget(widget); }}>Upload your photos</button>
+        </form>
+      </div>
+    )
+  }
+};
 
 export default AddAnswerModal;

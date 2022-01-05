e9d691213b1c7a17c61f8ed4ccad81d8dcd2aae5
Daniel <tawata.daniel@gmail.com>
2022-01-05 10:59:22 -0800

    Finished zoom in feature

--- client/Overview/Overview.jsx
+++ client/Overview/Overview.jsx
@@ -22,12 +22,89 @@ const Overview = (props) => {
   return (
     <div id="product-overview">
       {expandedView ? <OverviewExpanded product={product} counter={counter} updateExpandedView={updateExpandedView} /> : ''}
+      {/* {expandedView ? <OverviewTest product={product} counter={counter} updateExpandedView={updateExpandedView} /> : ''} */}
       <OverviewAnnouncement />
       <div className="po-flex">
         <OverviewGallery product={product} expandedCounter={counter} updateExpandedView={updateExpandedView} />
         <OverviewDetails product={product} updateProduct={updateProduct} />
       </div>
       <OverviewDescription product={product} />
+    </div>
+  );
+};
+
+const OverviewTest = (props) => {
+
+  const handleMouseMove = (e) => {
+    const container = document.querySelectorAll('.test-slide-image')[0];
+    let width = container.offsetWidth;
+    let height = container.offsetHeight;
+    let mouseX = e.nativeEvent.offsetX;
+    let mouseY = e.nativeEvent.offsetY;
+    console.log(e);
+    let bgPosX = (mouseX / width) * 100;
+    let bgPosY = (mouseY / height) * 100;
+    container.style.backgroundSize = '150%';
+    container.style.backgroundPosition = `${bgPosX}% ${bgPosY}%`;
+    console.log(container);
+  };
+
+  const handleMouseOut = (e) => {
+    const container = document.querySelectorAll('.test-slide-image')[0];
+    container.style.backgroundSize = '100%';
+  };
+
+  const moveSlideReel = (val) => {
+
+    const lastIndex = props.product.photos.length + 1;
+    if (props.counter.current === 0 || props.counter.current === lastIndex) {
+      return;
+    }
+
+    const slideReel = document.querySelector('.test-slide-reel');
+    props.counter.current = val;
+
+    let move = (props.counter.current * -100) + '%';
+    slideReel.style.transition = 'all 0.4s ease-in-out';
+    slideReel.style.transform = 'translateX(' + move + ')';
+
+    // let bubbleMove = (props.counter.current - 1) * 50;
+    // if (props.counter.current === lastIndex) {
+    //   bubbleMove = 0;
+    // } else if (props.counter.current === 0) {
+    //   bubbleMove = (lastIndex - 2) * 50;
+    // }
+    // const bubble = document.querySelector('.po-icon-bubble-selected');
+    // bubble.style.transform = 'translateX(' + bubbleMove + 'px)';
+
+    slideReel.addEventListener('transitionend', () => {
+      if (props.counter.current === 0) {
+        props.counter.current = lastIndex - 1;
+      } else if (props.counter.current === lastIndex) {
+        props.counter.current = 1;
+      }
+
+      move = (props.counter.current * -100) + '%';
+      slideReel.style.transition = 'none';
+      slideReel.style.transform = 'translateX(' + move + ')';
+
+      // bubbleMove = (props.counter.current - 1) * 50;
+      // bubble.style.transform = 'translateX(' + bubbleMove + 'px)';
+    });
+
+  };
+
+  return (
+    <div className="test-modal">
+      <div className="test-inner-modal">
+        <div onClick={() => { moveSlideReel(props.counter.current - 1); }} className="po-expanded-left-arrow"><i className="fas fa-arrow-left"></i></div>
+        <div onClick={() => { moveSlideReel(props.counter.current + 1); }} className="po-expanded-right-arrow"><i className="fas fa-arrow-right"></i></div>
+        <div className="test-slide-reel" onMouseMove={(e) => { handleMouseMove(e); }} onMouseLeave={(e) => { handleMouseOut(e); }}>
+          <div className="test-slide-image"></div>
+          <div className="test-slide-image"></div>
+          <div className="test-slide-image"></div>
+        </div>
+      </div>
     </div>
   );
 };
--- client/Overview/OverviewExpanded.jsx
+++ client/Overview/OverviewExpanded.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const OverviewExpanded = (props) => {
 
+  const [zoom, setZoom] = useState(false);
 
   const moveSlideReel = (val) => {
-
     const lastIndex = props.product.photos.length + 1;
     if (props.counter.current === 0 || props.counter.current === lastIndex) {
       return;
@@ -14,7 +14,7 @@ const OverviewExpanded = (props) => {
     props.counter.current = val;
 
     let move = (props.counter.current * -100) + '%';
-    slideReel.style.transition = 'all 0.4s ease-in-out';
+    slideReel.style.transition = 'all 0.8s ease-in-out';
     slideReel.style.transform = 'translateX(' + move + ')';
 
     let bubbleMove = (props.counter.current - 1) * 50;
@@ -40,7 +40,6 @@ const OverviewExpanded = (props) => {
       bubbleMove = (props.counter.current - 1) * 50;
       bubble.style.transform = 'translateX(' + bubbleMove + 'px)';
     });
-
   };
 
   useEffect(() => {
@@ -50,15 +49,76 @@ const OverviewExpanded = (props) => {
     slideReel.style.transform = 'translateX(' + move + ')';
   }, []);
 
+  const toggleZoom = (e) => {
+    if (e.target.classList.value !== 'po-expanded-slide') {
+      return;
+    }
+    const container = document.querySelectorAll('.po-expanded-slide')[props.counter.current];
+    container.style.backgroundSize = '100%';
+    container.style.backgroundPosition = 'center';
+    setZoom(!zoom);
+  };
+
   return (
     <div className="po-expanded" onClick={(e) => { props.updateExpandedView(e); }}>
-      <div className="po-expanded-inner">
-        <div onClick={() => { moveSlideReel(props.counter.current - 1); }} className="po-expanded-left-arrow"><i className="fas fa-arrow-left"></i></div>
-        <div onClick={() => { moveSlideReel(props.counter.current + 1); }} className="po-expanded-right-arrow"><i className="fas fa-arrow-right"></i></div>
-        <OverviewExpandedSlideReel product={props.product} />
-        <OverviewIconBubbles product={props.product} counter={props.counter.current} moveSlideReel={moveSlideReel} />
+      <div className="po-expanded-inner" onClick={(e) => { toggleZoom(e); }}>
+        {zoom ? '' : <div onClick={() => { moveSlideReel(props.counter.current - 1); }} className="po-expanded-left-arrow"><i className="fas fa-arrow-left"></i></div>}
+        {zoom ? '' : <div onClick={() => { moveSlideReel(props.counter.current + 1); }} className="po-expanded-right-arrow"><i className="fas fa-arrow-right"></i></div>}
+        <OverviewExpandedSlideReel product={props.product} counter={props.counter} zoom={zoom} toggleZoom={toggleZoom} />
+        {zoom ? '' : <OverviewIconBubbles product={props.product} counter={props.counter.current} moveSlideReel={moveSlideReel} />}
       </div>
     </div>
+  );
+};
+
+const OverviewExpandedSlideReel = (props) => {
+  const handleMouseMove = (e) => {
+    if (!props.zoom) {
+      return;
+    }
+    const container = document.querySelectorAll('.po-expanded-slide')[props.counter.current];
+    let width = container.offsetWidth;
+    let height = container.offsetHeight;
+    let mouseX = e.nativeEvent.offsetX;
+    let mouseY = e.nativeEvent.offsetY;
+    let bgPosX = (mouseX / width) * 100;
+    let bgPosY = (mouseY / height) * 100;
+    container.style.backgroundSize = '250%';
+    container.style.backgroundPosition = `${bgPosX}% ${bgPosY}%`;
+  };
+
+  const handleMouseLeave = (e) => {
+    const container = document.querySelectorAll('.po-expanded-slide')[props.counter.current];
+    container.style.backgroundSize = '100%';
+    container.style.backgroundPosition = 'center';
+  };
+
+  return (
+    <div className="po-expanded-slide-reel" onMouseMove={(e) => { handleMouseMove(e); }} onMouseLeave={(e) => { handleMouseLeave(e); }} onClick={props.toggleZoom}>
+      <OverviewExpandedSlide key="last" photo={props.product.photos.length > 0 ? props.product.photos[props.product.photos.length - 1] : {url: ''}} />
+      {props.product.photos.map((photo, index) => {
+        return <OverviewExpandedSlide key={index} photo={photo} />;
+      })}
+      <OverviewExpandedSlide key="first" photo={props.product.photos.length > 0 ? props.product.photos[0] : {url: ''}} />
+    </div>
+  );
+};
+
+const OverviewExpandedSlide = (props) => {
+
+  let styling = {
+    'flexShrink': '0',
+    'width': '100%',
+    'paddingTop': 'calc(100% / (16/12))',
+    'background': 'url(' + props.photo.url + ')',
+    'backgroundPosition': 'center',
+    'backgroundRepeat': 'no-repeat',
+    'backgroundSize': '100%',
+    'cursor': 'pointer'
+  };
+
+  return (
+    <div className="po-expanded-slide" style={styling}></div>
   );
 };
 
@@ -82,31 +142,8 @@ const OverviewIconBubble = (props) => {
     props.moveSlideReel(props.index);
   };
 
-
   return (
     <div onClick={() => { handleClick(); }} className="po-icon-bubble" ref={bubble}></div>
-  );
-};
-
-const OverviewExpandedSlideReel = (props) => {
-  return (
-    <div className="po-expanded-slide-reel">
-      <OverviewExpandedSlide key="last" photo={props.product.photos.length > 0 ? props.product.photos[props.product.photos.length - 1] : {url: ''}} />
-      {
-        props.product.photos.map((photo, index) => {
-          return <OverviewExpandedSlide key={index} photo={photo} />;
-        })
-      }
-      <OverviewExpandedSlide key="first" photo={props.product.photos.length > 0 ? props.product.photos[0] : {url: ''}} />
-    </div>
-  );
-};
-
-const OverviewExpandedSlide = (props) => {
-  return (
-    <div className="po-expanded-img-container">
-      <img src={props.photo.url} />
-    </div>
   );
 };
 

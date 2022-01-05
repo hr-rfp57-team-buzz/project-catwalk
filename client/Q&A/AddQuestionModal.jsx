aa8c4244320380ba5e1a9cc7a7bbc340d32ce356
Alex Romero <alexromeroreyes09@gmail.com>
2022-01-05 12:05:47 -0800

    Completed 'Add Question' modal layout

--- client/Q&A/AddQuestionModal.jsx
+++ client/Q&A/AddQuestionModal.jsx
@@ -9,7 +9,15 @@ let AddQModal = (props) => (
       <textarea name="question" id="id" required='required'
             minLength='10' maxLength='1000'
             rows="10" cols='100'>
-          </textarea>
+      </textarea>
+      <label>What is your nickname?</label>
+      <input maxLength='60' name="Nickname" id="nickname" required='required'placeholder='Example: jack543!' type='text'/>
+      <h5>For privacy reasons, do not use yout full name or email address</h5>
+      <label>Your email:</label>
+          <input pattern="/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/'"
+            name='Email' id='email' required='email' placeholder='Example: jack@email.com' type='text' maxLength='60'/>
+          <h5>For authentication reasons, you will not be emailed</h5>
+          <input type='submit'/>
     </form>
   </div>
 )
